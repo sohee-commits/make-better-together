d6819980f5b6c67c76f3047320aacba0195988b0
소희 <calapenaw@yandex.ru>
2024-09-16 11:34:14 +0500

    Add files via upload

--- scripts/main.js
+++ scripts/main.js
@@ -1,25 +1,25 @@
 // подсвечивание навигации
 
-const indexLink = document.querySelectorAll(`nav a[href="index.html"]`);
-const userLink = document.querySelectorAll(`nav a[href="user.html"]`);
+const indexLink = document.querySelectorAll(`nav a[href="index.php"]`);
+const userLink = document.querySelectorAll(`nav a[href="user.php"]`);
 
 // страницы только две, мб открыта index/без названия
 // поэтому идем от противоположного — user
-if (window.location.href.includes(`user.html`)) {
-	indexLink.forEach((link) => {
-		link.classList.remove(`colored`);
-	});
-	userLink.forEach((link) => {
-		link.classList.add(`colored`);
-	});
-	// а если не user (index/без названия), то подсвечиваем главная
+if (window.location.href.includes(`user.php`)) {
+  indexLink.forEach((link) => {
+    link.classList.remove(`colored`);
+  });
+  userLink.forEach((link) => {
+    link.classList.add(`colored`);
+  });
+  // а если не user (index/без названия), то подсвечиваем главная
 } else {
-	indexLink.forEach((link) => {
-		link.classList.add(`colored`);
-	});
-	userLink.forEach((link) => {
-		link.classList.remove(`colored`);
-	});
+  indexLink.forEach((link) => {
+    link.classList.add(`colored`);
+  });
+  userLink.forEach((link) => {
+    link.classList.remove(`colored`);
+  });
 }
 
 // открытие навигации
@@ -28,5 +28,5 @@ const openNav = document.querySelector(`#open-nav`);
 const nav = document.querySelector(`header#header-mobile ul`);
 
 openNav.addEventListener(`click`, () => {
-	nav.classList.toggle(`hidden`);
+  nav.classList.toggle(`hidden`);
 });
