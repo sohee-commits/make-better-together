f7ba91976eb3e4c4de6ea6f0e89695d9f3cba078
소희 <calapenaw@yandex.ru>
2024-09-18 14:09:26 +0500

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+// подсвечивание навигации
+
+const indexLink = document.querySelectorAll(`nav a[href="index.php"]`);
+const userLink = document.querySelectorAll(`nav a[href="user.php"]`);
+
+// страницы только две, мб открыта index/без названия
+// поэтому идем от противоположного — user
+if (window.location.href.includes(`user.php`)) {
+  indexLink.forEach((link) => {
+    link.classList.remove(`colored`);
+  });
+  userLink.forEach((link) => {
+    link.classList.add(`colored`);
+  });
+  // а если не user (index/без названия), то подсвечиваем главная
+} else {
+  indexLink.forEach((link) => {
+    link.classList.add(`colored`);
+  });
+  userLink.forEach((link) => {
+    link.classList.remove(`colored`);
+  });
+}
+
+// открытие навигации
+
+const openNav = document.querySelector(`#open-nav`);
+const nav = document.querySelector(`header#header-mobile ul`);
+
+openNav.addEventListener(`click`, () => {
+  nav.classList.toggle(`hidden`);
+});
